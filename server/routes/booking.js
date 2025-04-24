ef9c985c5c4cd331ca5968d7d237fe4adb8e6127
raahimausman <64039189+raahimausman@users.noreply.github.com>
2025-04-25 01:33:49 +0500

    Delete server directory

--- server/routes/booking.js
+++
@@ -1,6 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { makeBooking, makePayment } = require("../controllers/booking");
-router.route('/create',makeBooking).post(makeBooking);
-router.route('/make-payment').post(makePayment);
-module.exports = router;
